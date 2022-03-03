061983b04715d4910c3cab6bff41f4d36811343b
Gregory Josephs <greg.josephs@gmail.com>
2022-03-03 09:50:20 -0500

    Added create account

--- home.js
+++ home.js
@@ -1,10 +1,17 @@
 
 // This document controls what is routed when Home is selected from the router
 function Home() {
-    const ctx = React.useContext(UserContext)
     return (
-        <h1>Home<br/>
-            {JSON.stringify(ctx)}
-        </h1>
+       <Card
+            txtcolor="black"
+            header="Bad Bank Landing Page"
+            title="Welcome to the Bank"
+            text="You can use this bank"
+            body="I don't really want to worry about making an image at this point because honestly what's the point of that in this exercise?"
+       
+       
+       
+       
+       />
     );
 }
