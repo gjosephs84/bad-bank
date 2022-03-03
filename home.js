04806c6df467959af2add8c809c39366c0e3a12e
Gregory Josephs <greg.josephs@gmail.com>
2022-03-03 07:36:56 -0500

    Added navbar and routing

--- home.js
+++ home.js
@@ -1,13 +1,7 @@
 
 // This document controls what is routed when Home is selected from the router
 function Home() {
-    // Here UserContext is being pulled into a local variable
-    const ctx = React.useContext(UserContext); 
     return (
-        <div>
-            <h3>Home Component</h3>
-            <p>Here we go {ctx.users}!!!!!</p>
-            
-        </div>
+        <h1>Home</h1>
     );
 }
--- index.js
+++ index.js
@@ -1,43 +1,18 @@
-/*  The purpose of this file is to handle the routing for our single page
-application (SPA). The following component is the parent for the whole
-application.
-*/
-// The following is a context variable to be shared between components
-const UserContext = React.createContext(null);
-const Route = ReactRouterDOM.Route;
-const Link = ReactRouterDOM.Link;
-const HashRouter = ReactRouterDOM.HashRouter;
-
 function Spa() {
     
 
     
 
     return (
-        // Returning the router defined above
-        // Wrapped in the UserContext
         <HashRouter>
-            <div>
-                <h1>Routing - Hello World</h1>
-                {/* 
-                
-                Create the links 
-                
-                */}
-                <Nav/>
-                <hr/>
-                {/* 
-                
-                Tell the links what to load? Read up more on this.
-                Also, wrapping in the UserContext so they all have access
-                
-                */}
-                <UserContext.Provider value={{users:['Gregory']}}>
-                    <Route path="/" exact component={Home}/>
-                    <Route path="/about/" component={About}/>
-                    <Route path="/products/" component={Products}/>
-                </UserContext.Provider>
-            </div>
+            <NavBar/>
+            <Route path="/" exact           component={Home}/>
+            <Route path="/createaccount/"   component={CreateAccount}/>
+            <Route path="/login/"           component={Login}/>
+            <Route path="/deposit/"         component={Deposit}/>
+            <Route path="/withdraw/"        component={Withdraw}/>
+            <Route path="/balance/"         component={Balance}/>
+            <Route path="/alldata/"         component={AllData}/>
         </HashRouter>
     );
 }
